d5f5a2985510fcbee79e3fc8d3ef92c3eb042d92
Louis <lpgauvin36@gmail.com>
2026-06-02 17:45:03 -0400

    Doit faire un diagramme

--- TechnologyTree.h
+++ TechnologyTree.h
@@ -7,7 +7,22 @@
 
 
 //all Technologie names
+enum class TechnologyTreeType {
+None,
+//need to make a graph to show the tree flow. For Military and Civil
+// -> KNIGHT TECH TREE <-
 
+// -> VIKING TECH TREE <-
+
+// -> SAMURAI TECH TREE <-
+
+
+};
+
+enum class TechnologieCategory {
+    MilitaryTech,
+    CivilTech
+};
 
 //Technologie Type (Military Focus, Civil Focus)
 struct TechnologyTree {
