22164b6fb213a3a08f128db66c92e5fb2d08a10a
Louis <lpgauvin36@gmail.com>
2026-06-02 16:25:00 -0400

    Start Techno Database

---
+++ TechnologyTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lpgau on 2026-06-02.
+//
+
+#include "TechnologyTree.h"
---
+++ TechnologyTree.h
@@ -0,0 +1,17 @@
+//
+// Created by lpgau on 2026-06-02.
+//
+
+#ifndef TOTALWAR2D_TECHNOLOGYTREE_H
+#define TOTALWAR2D_TECHNOLOGYTREE_H
+
+
+//all Technologie names
+
+
+//Technologie Type (Military Focus, Civil Focus)
+struct TechnologyTree {
+};
+
+
+#endif //TOTALWAR2D_TECHNOLOGYTREE_H
