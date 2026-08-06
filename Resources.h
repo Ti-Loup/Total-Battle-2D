2ad37a05fe73f1d4bd30597e89430f637c4902a1
Louis <lpgauvin36@gmail.com>
2026-08-06 10:06:37 -0400

    ResourceType Per mines

--- Resources.h
+++ Resources.h
@@ -11,7 +11,13 @@ Script to get all the resources that the buildings produces.
 
 //Types of Resources
 enum class ResourceType {
-    Fish
+    Fish,
+    Iron,
+    Gold,
+    Copper,
+    Silver,
+    Tin,
+    Lumber
 };
 
 //If Raw or Transformed Goods
@@ -31,8 +37,13 @@ struct ResourceData{
 
 inline const std::unordered_map<ResourceType, ResourceData>& GetResourceDatabase() {
     static const std::unordered_map<ResourceType, ResourceData> database = {
-        { ResourceType::Fish, { "Fish", "Caught by fishing ports. Feeds the population and can be traded.", "assets/Resources/Fish.png", ResourceCategory::Raw, 2} }
-
+        { ResourceType::Fish, { "Fish", "Caught by fishing ports. Feeds the population and can be traded.", "assets/Resources/Fish.png", ResourceCategory::Raw, 5}}
+        { ResourceType::Iron, { "Iron", "From iron mines. Good to produce tools in the forge or traded.", "assets/Resources/Iron.png", ResourceCategory::Raw, 10}}
+        { ResourceType::Gold, { "Gold", "From gold mines. These are the most valuables goods to craft Jewelley.", "assets/Resources/Gold.png", ResourceCategory::Raw, 25}}
+        { ResourceType::Copper, { "Copper", "From copper mines. Valuable to make Copper Jewelley.", "assets/Resources/Copper.png", ResourceCategory::Raw, 12}}
+        { ResourceType::Silver, { "Silver", "From silver mines. Silver is always usefull to create those coins.", "assets/Resources/Silver.png", ResourceCategory::Raw, 20}}
+        { ResourceType::Tin, { "Tin", "From tin mines. Tin is used to make tin coins.", "assets/Resources/Tin.png", ResourceCategory::Raw, 16}}
+        { ResourceType::Lumber, { "Lumber", "Lumber, from the forest is very important to build fournitures.", "assets/Resources/Lumber.png", ResourceCategory::Raw, 8}}
     };
     return database;
 }
